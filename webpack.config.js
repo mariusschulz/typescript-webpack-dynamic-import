b7f3154fa3b706cb4942a36bf1afc0393af3a694
Trevor Nichols <trevor@ml.net.au>
2018-04-22 18:52:07 +1000

    Update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/main.ts",
-
+mode: "development",
     output: {
         path: path.join(__dirname, "dist3"),
         filename: "[name].bundle.js",
@@ -14,7 +14,7 @@ module.exports = {
     },
 
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.ts$/,
                 include: path.join(__dirname, "src"),
